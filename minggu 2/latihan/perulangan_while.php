84739241e5e6ce66d5f9bdfa8fffebeed0fc5cf0
user <beryl.naga@gmail.com>
2022-03-09 12:14:37 +0700

    Initial Commit

---
+++ minggu 2/latihan/perulangan_while.php
@@ -0,0 +1,14 @@
+<html><body>
+    <?php
+    $i = 1;
+    while ($i < 6){
+        $j=1;
+        while ($j <= $i){
+            echo "$i";
+            $j++;
+        }
+        echo "<br>";
+        $i++;
+    }
+    ?>
+</body></html>
