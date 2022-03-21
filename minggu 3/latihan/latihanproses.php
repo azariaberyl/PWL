e5ed523f5faabbc6e8a39b638b89f6e797d70e8f
user <beryl.naga@gmail.com>
2022-03-21 15:18:24 +0700

    Initial Commit

---
+++ minggu 3/latihan/latihanproses.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil Form</title>
+    <style>
+        th {
+            padding: 10px;
+        }
+    </style>
+</head>
+<?php
+    if(isset($_POST["submit"])){
+        $tugas = $_POST["tugas"];
+        $uts = $_POST["uts"];
+        $nim = $_POST["nim"];
+        $uas = $_POST["uas"];
+
+        $prodi = $_POST["prodi"];
+        
+    }
+?>
+<body>
+    <table>
+        <tr>
+            <th>Program Studi</th>
+            <th>NIM</th>
+            <th>Nilai Akhir</th>
+            <th>STATUS</th>
+            <th>Catatan Khusus</th>
+        </tr>
+        <tr>
+            <!-- Prodi -->
+            <td><?php
+            switch ($prodi) {
+                case 'A11':
+                    echo "Teknik Informatika S1";
+                    break;
+                case 'A12':
+                    echo "Sistem Informasi S1";
+                    break;
+                case 'A22':
+                    echo "Teknik Informatika D3";
+                    break;
+                
+                default:
+                    echo "Error";
+                    break;
+            }
+            ?></td>
+            <!-- Nim -->
+            <td>
+                <?php echo "$nim "; ?>
+            </td>
+            <!-- Nilai Akhir -->
+            <td>
+                <?php
+                $na = (0.4*$tugas) + (0.3*$uts) + (0.3*$uas);
+                echo $na;
+                ?>
+            </td>
+            <!-- Status -->
+            <td>
+                <?php
+                if ($na >= 60 ){
+                    echo "Lulus";
+                } else {
+                    echo "Gagal";
+                }
+                 ?>
+            </td>
+            <td>
+                <ul>
+                    <?php
+                    if(isset($_POST["kehadiran"])){
+                        $kehadiran = $_POST["kehadiran"];
+                        echo "<li>$kehadiran</li>";
+                    }
+                    if(isset($_POST["interaktif"])){
+                        $interaktif = $_POST["interaktif"];
+                        echo "<li>$interaktif</li>";
+                    }
+                    if(isset($_POST["tidakTerlambat"])){
+                        $tidakTerlambat = $_POST["tidakTerlambat"];
+                        echo "<li>$tidakTerlambat</li>";
+                    }
+                    ?>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
